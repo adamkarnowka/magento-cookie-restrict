0ca4c9f63ce54a50bffcc4e80d14b6701d70a2ce
Adam Karnowka <adam.karnowka@creativestyle.pl>
2014-01-18 10:07:14 +0100

    [FIX] Fixed multistore support (getStore() in Mage::getUrl()) for getting correct path for specific store

--- app/code/local/Creativestyle/Dev/Block/Adminhtml/Notification.php
+++ app/code/local/Creativestyle/Dev/Block/Adminhtml/Notification.php
@@ -3,6 +3,6 @@
 class Creativestyle_Dev_Block_Adminhtml_Notification extends Mage_Adminhtml_Block_Template
 {
     public function isProtected(){
-        return Mage::getStoreConfig('dev/csoptions/cookie_protection_enabled');
+        return Mage::getStoreConfig('dev/csoptions/cookie_protection_enabled',  Mage::app()->getRequest()->getParam('store'));
     }
 }
