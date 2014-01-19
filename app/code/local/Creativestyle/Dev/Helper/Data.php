c3a0a0befe5194b0f8597050a3a8d3d1fb118dd4
Adam Karnowka <adam.karnowka@creativestyle.pl>
2014-01-19 16:28:01 +0100

    [UPDATE] Added phpdoc comments

--- app/code/local/Creativestyle/Dev/Helper/Data.php
+++ app/code/local/Creativestyle/Dev/Helper/Data.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: Adam
+ * User: Adam Karnowka
  * Date: 09.01.14
  * Time: 12:29
- * To change this template use File | Settings | File Templates.
- */ 
+ */
 class Creativestyle_Dev_Helper_Data extends Mage_Core_Helper_Abstract {
 
 }
--- app/code/local/Creativestyle/Dev/sql/dev_setup/mysql4-install-0.1.1.php
+++ app/code/local/Creativestyle/Dev/sql/dev_setup/mysql4-install-0.1.1.php
@@ -7,6 +7,9 @@ Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
 $html = '<div style="-webkit-box-shadow: 0 18px 16px -16px black; -moz-box-shadow: 0 18px 16px -16px black; box-shadow: 0 18px 16px -16px black; ;width: 800px; height: 230px; padding: 30px; text-shadow: 0px 2px #eee2be; text-align: center; font-size: 45px; font-family: Helvetica, Arial; margin: auto; margin-top: 100px; padding-top: 120px; background-color: #fff9e9; border: 1px solid #eee2be;">Sorry, this project is still in developement, please come back later!<br /><br /> <span style="font-size: 11px;"><a style="text-decoration: none; color: black;" href="http://www.creativestyle.de">www.creativestyle.de</a></span></div>';
 
+/**
+ * Insertind default static block
+ */
 $staticBlock = array(
     'title' => 'Access restricted [EN]',
     'identifier' => 'access-restricted',
