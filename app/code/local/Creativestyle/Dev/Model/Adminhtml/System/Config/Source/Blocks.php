5f7546eccbd0741146ad2070527669d969088f53
Adam Karnowka <adam.karnowka@creativestyle.pl>
2014-01-19 16:10:07 +0100

    [UPDATE] Added phpdoc comments

--- app/code/local/Creativestyle/Dev/Model/Adminhtml/System/Config/Source/Blocks.php
+++ app/code/local/Creativestyle/Dev/Model/Adminhtml/System/Config/Source/Blocks.php
@@ -1,15 +1,17 @@
 <?php
-    class Creativestyle_Dev_Model_Adminhtml_System_Config_Source_Blocks
+class Creativestyle_Dev_Model_Adminhtml_System_Config_Source_Blocks
 {
     protected $_options;
 
+    /**Returns option array with listed all CMS static blocks
+     * @return mixed
+     */
     public function toOptionArray()
     {
         if (!$this->_options) {
-            $this->_options = Mage::getResourceModel('cms/block_collection')
-            ->load()->toOptionArray();
+            $this->_options = Mage::getResourceModel('cms/block_collection')->load()->toOptionArray();
         }
-        return $this->_options;
-}
 
+        return $this->_options;
+    }
 }
